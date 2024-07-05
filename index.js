4ddce99b0de6551e3781a1bd8e48e6892a1c9e12
Leowq811 <veronicasam590@gmail.com>
2024-07-05 12:37:30 -0600

    Initial commit

---
+++ index.js
@@ -0,0 +1,4 @@
+<html><head></head><body><script type="text/javascript" id="dcoder_script">function showPasswordForm() {
+    document.getElementById('email').classList.add('hidden');
+    document.getElementById('password').classList.remove('hidden');
+}</script></body></html>
